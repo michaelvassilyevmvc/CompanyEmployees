dfd37b5b63948b6113e4b817077467df8cd4c5dd
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-06-19 08:11:47 +0600

    Lesson9 has been finished. Post and Get collection via httprequest

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -11,5 +11,7 @@ namespace Contracts
     {
         IEnumerable<Company> GetAllCompanies(bool trackChanges);
         Company GetCompany(Guid companyId, bool trackChanges);
+        IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+        void CreateCompany(Company company);
     }
 }
