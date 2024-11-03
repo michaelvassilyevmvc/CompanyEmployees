85d40b465489ea560226446c82e278cf8177c30c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2024-11-04 00:30:22 +0500

    Add project files.

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        /// <summary>
+        /// Настройка для возможности работать приложению между доменами
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services) => 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                                 builder.AllowAnyOrigin()
+                                        .AllowAnyMethod()
+                                        .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
