71a9978db9c1cb7c117b2e6c0edc6cc68f926659
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2024-11-10 10:57:52 +0500

    Создан слой Service и слой Repository

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Company
+    {
+        [Column("CompanyId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Company address is a requered field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Address is 60 characters")]
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Country { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
