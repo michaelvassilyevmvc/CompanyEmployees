17473009ad79796a13f63646fb57e47fd096bc60
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2024-11-15 23:27:14 +0500

    Созданы классы исключений для Get запросов. Добавлены запросы для обработки получения списка и одиночного значения.

--- CompanyEmployees/GlobalExceptionHandler.cs
+++ CompanyEmployees/GlobalExceptionHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using System.Net;
 
@@ -17,16 +18,20 @@ namespace CompanyEmployees
             Exception exception,
             CancellationToken cancellationToken)
         {
-            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
             httpContext.Response.ContentType = "application/json";
 
             var contextFeature = httpContext.Features.Get<IExceptionHandlerFeature>();
-            if (contextFeature != null) { 
+            if (contextFeature != null) {
+                httpContext.Response.StatusCode = contextFeature.Error switch
+                {
+                    NotFoundException => StatusCodes.Status404NotFound,
+                    _ => StatusCodes.Status500InternalServerError
+                };
                 _logger.LogError($"Something went wrong: {exception.Message}");
 
                 await httpContext.Response.WriteAsync(new ErrorDetails(){
                     StatusCode = httpContext.Response.StatusCode,
-                    Message = "Internal Server Error."
+                    Message = contextFeature.Error.Message
                 }.ToString());
             }
             return true;
---
+++ Shared/DataTransferObjects/EmployeeDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.DataTransferObjects
+{
+    public record class EmployeeDto(Guid id, string Name, int Age, string Position);
+}
